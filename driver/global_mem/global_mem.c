78eaeff82320f4b543b26faa2b1f1264578a6aef
huangliang <584782297@qq.com>
2023-06-18 07:02:01 -0700

    support two devices

--- driver/global_mem/global_mem.c
+++ driver/global_mem/global_mem.c
@@ -26,7 +26,10 @@ struct globalmem_dev *globalmem_devp;
 
 int globalmem_open(struct inode *inode, struct file *filp)
 {
-    filp->private_data = globalmem_devp;
+    struct globalmem_dev *dev;
+
+    dev = container_of(inode->i_cdev, struct globalmem_dev, cdev);
+    filp->private_data = dev;
     return 0;
 }
 
@@ -145,8 +148,9 @@ static const struct file_operations globalmem_fops = {
 
 static void globalmem_setup_dev(struct globalmem_dev *dev, int index)
 {
-    int err, devno = MKDEV(globalmem_major, 0);
+    int err, devno = MKDEV(globalmem_major, index);
 
+    printk("dev = %p, index = %d\n", dev, index);
     cdev_init(&dev->cdev, &globalmem_fops);
     dev->cdev.owner = THIS_MODULE;
     dev->cdev.ops = &globalmem_fops;
@@ -162,9 +166,9 @@ int globalmem_init(void)
     dev_t devno = MKDEV(globalmem_major, 0);
 
     if (globalmem_major) {
-        result = register_chrdev_region(devno, 1, "globalmem");
+        result = register_chrdev_region(devno, 2, "globalmem");
     } else {
-        result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        result = alloc_chrdev_region(&devno, 0, 2, "globalmem");
         globalmem_major = MAJOR(devno);
     }
 
@@ -172,14 +176,15 @@ int globalmem_init(void)
         return result;
     }
 
-    globalmem_devp = kmalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+    globalmem_devp = kmalloc(2 * sizeof(struct globalmem_dev), GFP_KERNEL);
     if (!globalmem_devp) {
         result = -ENOMEM;
         goto fail_malloc;
     }
-    memset(globalmem_devp, 0, sizeof(struct globalmem_dev));
+    memset(globalmem_devp, 0, 2 * sizeof(struct globalmem_dev));
 
-    globalmem_setup_dev(globalmem_devp, 0);
+    globalmem_setup_dev(&globalmem_devp[0], 0);
+    globalmem_setup_dev(&globalmem_devp[1], 1);
     goto success;
 
 fail_malloc:
@@ -191,8 +196,10 @@ success:
 
 void globalmem_exit(void)
 {
-    cdev_del(&globalmem_devp->cdev);
-    unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
+    cdev_del(&(globalmem_devp[0].cdev));
+    cdev_del(&(globalmem_devp[1].cdev));
+    kfree(globalmem_devp);
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), 2);
 }
 
 module_param(globalmem_major, int, S_IRUGO);
