2aaacc94f87a58b0b6ddc1fc0ab050eee6d57bc3
huangliang <584782297@qq.com>
2023-08-09 20:17:56 -0700

    add generics

---
+++ rust/generics/src/main.rs
@@ -0,0 +1,53 @@
+fn add_i8(a: i8, b: i8) -> i8 {
+    a + b
+}
+
+fn add_i32(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn add_f64(a: f64, b: f64) -> f64 {
+    a + b
+}
+
+fn add<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    println!("add i8: {}", add_i8(2i8, 3i8));
+    println!("add i32: {}", add_i32(20, 30));
+    println!("add f64: {}", add_f64(1.23, 1.23));
+
+    println!("add i8: {}", add(2i8, 3i8));
+    println!("add i32: {}", add(20, 30));
+    println!("add f64: {}", add(1.23, 1.23));
+
+    let integer = Point {x: 5, y: 10};
+    let float = Point {x: 1.0, y: 4.0};
+    let p = Point {x: 1, y: 1.1};
+
+    println!("p.x = {}", p.x());
+}
